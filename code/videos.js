ac1786c836ab811fe15da1ee12b2759bd8b23568
Adir <slutzkis@gmail.com>
2023-02-22 14:49:53 +0200

    updates

--- code/videos.js
+++ code/videos.js
@@ -1,13 +1,17 @@
 function videosOnScroll(){
     scrollPos = document.getElementById("wrapper").scrollTop;
+    scrollOffset = 400;
     var x = document.querySelectorAll("video");
     var i;
     for (i = 0; i < x.length; i++) {
-        if(scrollPos > x[i].scrollHeight){
-            x[i].pause();
+        if(scrollPos > x[i].parentElement.offsetTop - scrollOffset && scrollPos < x[i].parentElement.offsetTop + x[i].parentElement.scrollTop + scrollOffset){
+            if(x[i].currentTime !== x[i].duration){
+                x[i].play();
+            }
+            console.log("in view: " + i);
         }else{
-            x[i].play();
+            x[i].currentTime = 0;
+            x[i].pause();
         }
     }
-    console.log(scrollPos)
 }
