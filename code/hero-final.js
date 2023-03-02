4965b9d1e94d189949917be810bf245793f5d942
Adir <slutzkis@gmail.com>
2023-03-02 18:42:13 +0200

    updates

--- code/hero-final.js
+++ code/hero-final.js
@@ -5,11 +5,11 @@ function moveFunc(e) {
     checkOrigX = e.pageX / percentX - 50;
     checkOrigY = e.pageY / percentY - 50;
 
-    checkX = e.pageX / percentX - 100;
+    checkX = e.pageX / percentX - 20;
     checkY = e.pageY / percentY - 50;
     checkX = checkX / -7;
     checkY = checkY / 7;
-    document.getElementById("kFont").style.setProperty('--shadowX', checkX+"px");
+    document.getElementById("kFont").style.setProperty('--shadowX', checkX*-1+"px");
     document.getElementById("kFont").style.setProperty('--shadowY', checkY+"px");
     if(checkY < 0){
         checkY = checkY * -1;
@@ -24,11 +24,11 @@ function moveFunc(e) {
     }
     document.getElementById("kFont").style.setProperty('--shadowFade', checkFade+"px");
 
-    checkX = e.pageX / percentX - 70;
+    checkX = e.pageX / percentX - 40;
     checkY = e.pageY / percentY - 50;
     checkX = checkX / -7;
     checkY = checkY / 7;
-    document.getElementById("iFont").style.setProperty('--shadowX', checkX+"px");
+    document.getElementById("iFont").style.setProperty('--shadowX', checkX*-1+"px");
     document.getElementById("iFont").style.setProperty('--shadowY', checkY+"px");
     if(checkY < 0){
         checkY = checkY * -1;
@@ -43,11 +43,11 @@ function moveFunc(e) {
     }
     document.getElementById("iFont").style.setProperty('--shadowFade', checkFade+"px");
 
-    checkX = e.pageX / percentX - 30;
+    checkX = e.pageX / percentX - 60;
     checkY = e.pageY / percentY - 50;
     checkX = checkX / -7;
     checkY = checkY / 7;
-    document.getElementById("dFont").style.setProperty('--shadowX', checkX+"px");
+    document.getElementById("dFont").style.setProperty('--shadowX', checkX*-1+"px");
     document.getElementById("dFont").style.setProperty('--shadowY', checkY+"px");
     if(checkY < 0){
         checkY = checkY * -1;
@@ -62,11 +62,11 @@ function moveFunc(e) {
     }
     document.getElementById("dFont").style.setProperty('--shadowFade', checkFade+"px");
 
-    checkX = e.pageX / percentX - 0;
+    checkX = e.pageX / percentX - 80;
     checkY = e.pageY / percentY - 50;
     checkX = checkX / -7;
     checkY = checkY / 7;
-    document.getElementById("oFont").style.setProperty('--shadowX', checkX+"px");
+    document.getElementById("oFont").style.setProperty('--shadowX', checkX*-1+"px");
     document.getElementById("oFont").style.setProperty('--shadowY', checkY+"px");
     if(checkY < 0){
         checkY = checkY * -1;
