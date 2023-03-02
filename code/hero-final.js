78782816aeb18bddbbc0f2dba9d0650be4f5c82b
Adir <slutzkis@gmail.com>
2023-03-02 14:51:08 +0200

    Create hero-final.js

---
+++ code/hero-final.js
@@ -0,0 +1,9 @@
+function moveFunc(e) {
+    percentX = document.getElementById("wrapper").offsetWidth / 100;
+    percentY = document.getElementById("wrapper").offsetHeight / 100;
+    document.getElementById("eyeObj").style.top = e.pageY / percentY + "%";
+    document.getElementById("eyeObj").style.left = e.pageX / percentX + "%";
+    checkX = e.pageX / percentX - 50;
+    checkY = e.pageY / percentY - 50;
+    document.getElementById("logoDiv").style.transform = "translate(" + checkX + "%, " + checkY + "%" + ")";
+}
