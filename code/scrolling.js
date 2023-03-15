c49a39d3a2334c8110d6909013dae295b4e6d570
Adir <slutzkis@gmail.com>
2023-03-15 10:28:55 +0200

    Update scrolling.js

--- code/scrolling.js
+++ code/scrolling.js
@@ -1,6 +1,12 @@
-document.getElementById("wrapper").addEventListener("scroll", scrollEndFunc);
+document.getElementById("wrapper").addEventListener("scroll", scrollingFunc);
+
+function scrollingFunc(){
+    clearTimeout(window.scrollEndTimer)
+    window.scrollEndTimer = setTimeout(scrollEndFunc, 200);
+}
 
 function scrollEndFunc() {
   console.log("scroll end");
+  
 }
 
