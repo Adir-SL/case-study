3b717692d18d93acad7c2db3ebb53351a1fc7cd0
Adir <slutzkis@gmail.com>
2023-03-29 14:38:38 +0300

    Update videos.js

--- code/videos.js
+++ code/videos.js
@@ -31,7 +31,7 @@ function videosOnScroll(){
 
     
 
-    if(scrollPos > document.getElementById("mobiles").offsetTop - (scrollOffset / 1.6) && scrollPos < document.getElementById("mobiles").offsetTop + document.getElementById("mobiles").offsetHeight){
+    if(scrollPos > document.getElementById("mobiles").offsetTop - (scrollOffset) && scrollPos < document.getElementById("mobiles").offsetTop + document.getElementById("mobiles").offsetHeight){
     }else{
         document.getElementById("mobiles").classList.add("anim-out");
         setTimeout(function(){
