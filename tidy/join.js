5e59bc4e1296eb6b2d044ae020126f7fe5fe6a53
Adir <slutzkis@gmail.com>
2023-04-30 15:59:46 +0300

    Update join.js

--- tidy/join.js
+++ tidy/join.js
@@ -3,64 +3,65 @@ function joinFunc(name, mail, position, company) {
     createNotionPage(name, mail, position, company);
 
 }
-
 function createNotionPage(name, mail, position, company) {
-    const url = 'https://api.notion.com/v1/pages';
-    const databaseId = '438db125d76c4173b624024e81134de5';
-    const apiKey = 'secret_Wa9Misu7LjcsQ6ZY06GeAsFxeTGrQhzuWHrdNqigc1L';
+    console.log(name, mail, position, company);
+    const url = "https://api.notion.com/v1/pages";
+    const proxiedUrl = `https:\/\/cors-anywhere.herokuapp.com/${url}`;
+    const databaseId = "438db125d76c4173b624024e81134de5";
+    const apiKey = "secret_Wa9Misu7LjcsQ6ZY06GeAsFxeTGrQhzuWHrdNqigc1L";
     const data = {
-      parent: {
-        database_id: databaseId,
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: name,
-              },
+        parent: {
+            database_id: databaseId,
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: name,
+                        },
+                    },
+                ],
             },
-          ],
-        },
-        Email: {
-          email: mail,
-        },
-        Position: {
-          rich_text: [
-            {
-              text: {
-                content: position,
-              },
+            Email: {
+                email: mail,
             },
-          ],
-        },
-        Company: {
-          rich_text: [
-            {
-              text: {
-                content: company,
-              },
+            Position: {
+                rich_text: [
+                    {
+                        text: {
+                            content: position,
+                        },
+                    },
+                ],
             },
-          ],
+            Company: {
+                rich_text: [
+                    {
+                        text: {
+                            content: company,
+                        },
+                    },
+                ],
+            },
         },
-      },
     };
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
-        'Notion-Version': '2022-04-30',
-      },
-      body: JSON.stringify(data),
+    fetch(proxiedUrl, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-02-22",
+        },
+        body: JSON.stringify(data),
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to create Notion page');
-        }
-        console.log('Notion page created successfully');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to create Notion page");
+            }
+            console.log("Notion page created successfully");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
