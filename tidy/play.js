b2188e68e1b27f787bd594badbef635432607d80
Adir <slutzkis@gmail.com>
2023-05-01 10:52:48 +0300

    Update play.js

--- tidy/play.js
+++ tidy/play.js
@@ -1,9 +1,11 @@
 function playFunc(e){
     if(window.playVar == 1){
         e.target.pause();
+        e.target.parentNode.getElementsByClassName("play-button")[0].style.opacity = 1;
         window.playVar = 0;
     }else{
         e.target.play();
+        e.target.parentNode.getElementsByClassName("play-button")[0].style.opacity = 0;
         window.playVar = 1;
     }
 }
