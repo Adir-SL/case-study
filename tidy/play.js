a9a01dfe61a916a43e10f996439e8b9e0a39ddb7
Adir <slutzkis@gmail.com>
2023-05-01 10:48:27 +0300

    Update play.js

--- tidy/play.js
+++ tidy/play.js
@@ -1,3 +1,5 @@
 function playFunc(e){
-    console.log(e.target);
+    // console.log(e.target);
+    // console.log(e.target.parentNode.getElementsByTagName("video")[0])
+    e.target.parentNode.getElementsByTagName("video")[0].play();
 }
