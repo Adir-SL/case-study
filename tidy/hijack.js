14d0136348e0ed4f8ccee11076e03d66a8fb42c0
Adir <slutzkis@gmail.com>
2023-05-02 14:24:37 +0300

    updates

--- tidy/hijack.js
+++ tidy/hijack.js
@@ -1,3 +1,23 @@
 function hijack(){
     console.log(document.getElementById("wrapper").scrollTop);
+    
+    if(document.getElementById("wrapper").scrollTop > document.getElementsByClassName("flex-container")[1].getBoundingClientRect().bottom){
+        resethijack();
+        document.getElementsByClassName("flex-container")[1].style.height = '100vh';
+    }else{
+        if(document.getElementById("wrapper").scrollTop > document.getElementsByClassName("flex-container")[0].getBoundingClientRect().bottom){
+            resethijack();
+            document.getElementsByClassName("flex-container")[0].style.height = '100vh';
+        }
+    }
+
+    
+}
+
+function resethijack(){
+    var x = document.getElementsByClassName("flex-container");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.height = "0";
+    }
 }
