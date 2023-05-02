9b86971937dd982617d3c6e48117b2ae2f5e4166
Adir <slutzkis@gmail.com>
2023-05-02 16:25:26 +0300

    updates

--- tidy/hijack.js
+++ tidy/hijack.js
@@ -18,9 +18,9 @@ function resethijack(){
     var x = document.getElementsByClassName("flex-container");
     var i;
     for (i = 0; i < x.length; i++) {
+        // x[i].style.transform = "scale("+ ((1000 - x[i].getBoundingClientRect().top)/1000) +")";
         // heightDeg = x[i].clientHeight /  180;
         // x[i].style.transform = "translateZ("+ Math.floor(x[i].getBoundingClientRect().top / 10) + "px)" + "rotateX("+ Math.floor((x[i].getBoundingClientRect().top / 100) * heightDeg * -1) + "deg)";
-        x[i].style.transform = "scale("+ ((1000 - x[i].getBoundingClientRect().top)/1000) +")";
         // if(x[i].getBoundingClientRect().top == 0){
         //     x[i].style.opacity = "1";
         // }else{
