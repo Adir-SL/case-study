a3356bb1fa6305cdc3cf56ef2a2a8f65b58d4f06
Adir <slutzkis@gmail.com>
2023-05-02 17:35:02 +0300

    updates

--- tidy/hijack.js
+++ tidy/hijack.js
@@ -18,18 +18,34 @@ function resethijack(){
     if(document.getElementsByClassName("flex-container")[0].getBoundingClientRect().top == 0){
         document.getElementsByTagName("details")[0].getElementsByTagName("summary")[0].click();
         document.getElementById("gif-videos").style.transform = "translateY(0)";
+        document.getElementById("gif-videos").getElementsByTagName("video")[0].style.opacity = 1;
+        document.getElementById("gif-videos").getElementsByTagName("video")[1].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[2].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[3].style.opacity = 0;
     }
     if(document.getElementsByClassName("flex-container")[1].getBoundingClientRect().top == 0){
         document.getElementsByTagName("details")[1].getElementsByTagName("summary")[0].click();
         document.getElementById("gif-videos").style.transform = "translateY(-25%)";
+        document.getElementById("gif-videos").getElementsByTagName("video")[0].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[1].style.opacity = 1;
+        document.getElementById("gif-videos").getElementsByTagName("video")[2].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[3].style.opacity = 0;
     }
     if(document.getElementsByClassName("flex-container")[2].getBoundingClientRect().top == 0){
         document.getElementsByTagName("details")[2].getElementsByTagName("summary")[0].click();
         document.getElementById("gif-videos").style.transform = "translateY(-50%)";
+        document.getElementById("gif-videos").getElementsByTagName("video")[0].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[1].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[2].style.opacity = 1;
+        document.getElementById("gif-videos").getElementsByTagName("video")[3].style.opacity = 0;
     }
     if(document.getElementsByClassName("flex-container")[3].getBoundingClientRect().top == 0){
         document.getElementsByTagName("details")[3].getElementsByTagName("summary")[0].click();
         document.getElementById("gif-videos").style.transform = "translateY(-75%)";
+        document.getElementById("gif-videos").getElementsByTagName("video")[0].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[1].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[2].style.opacity = 0;
+        document.getElementById("gif-videos").getElementsByTagName("video")[3].style.opacity = 1;
     }
 
 
