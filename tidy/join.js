87d0d75d9778ecc07d153041b859943a32858480
Adir <slutzkis@gmail.com>
2023-05-03 14:16:55 +0300

    Update join.js

--- tidy/join.js
+++ tidy/join.js
@@ -1,63 +1,70 @@
+function notionSuccess(data){
+  console.log(data.object);
+}
+
 function createNotionPage(title, email, position, company) {
-    const data = JSON.stringify({
-      parent: {
-        database_id: "59b459b8152e4a808a80998126fc27ff",
-      },
-  
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: title,
-              },
+  const data = JSON.stringify({
+    parent: {
+      database_id: "59b459b8152e4a808a80998126fc27ff",
+    },
+
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: title,
             },
-          ],
-        },
-        Email: {
-          email: email,
-        },
-        Position: {
-          rich_text: [
-            {
-              text: {
-                content: position,
-              },
+          },
+        ],
+      },
+      Email: {
+        email: email,
+      },
+      Position: {
+        rich_text: [
+          {
+            text: {
+              content: position,
             },
-          ],
-        },
-        Company: {
-          rich_text: [
-            {
-              text: {
-                content: company,
-              },
+          },
+        ],
+      },
+      Company: {
+        rich_text: [
+          {
+            text: {
+              content: company,
             },
-          ],
-        },
+          },
+        ],
       },
-    });
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Notion-Version": "2021-08-16",
-      },
-      body: JSON.stringify(data),
-      cache: "no-cache",
-    };
-  
-    fetch("https://corsproxy.io/?https://api.notion.com/v1/pages", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Notion-Version": "2021-05-13",
-        Authorization:
-          "Bearer secret_LLZvVuOqXWfvNFjURcdzr57pwx53Y6TlZjs2zIyTpLt",
-      },
-      body: data,
+    },
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Notion-Version": "2021-08-16",
+    },
+    body: JSON.stringify(data),
+    cache: "no-cache",
+  };
+
+  fetch("https://corsproxy.io/?https://api.notion.com/v1/pages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Notion-Version": "2021-05-13",
+      Authorization:
+        "Bearer secret_LLZvVuOqXWfvNFjURcdzr57pwx53Y6TlZjs2zIyTpLt",
+    },
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((data) => { 
+      console.log("data", data)
+      notionSuccess(data);
     })
-      .then((res) => res.json())
-      .then((data) => console.log("data", data))
-      .catch((err) => console.log("error", err));
-  }
+    .catch((err) => console.log("error", err));
+}
