b579b10807324ff383c87567b024a601054f42ed
Adir <slutzkis@gmail.com>
2023-06-06 11:40:23 +0300

    updates

--- code/headerScroll.js
+++ code/headerScroll.js
@@ -4,6 +4,11 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.opacity = '0';
+        if(document.getElementsByClassName('firstSection')[0].getBoundingClientRect().bottom < 0){
+            document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.backgroundImage = 'url(images/new-k.gif)';
+        }else{
+            document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.backgroundImage = 'url(images/new-k-invert.gif)';
+        }
     }else{
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '1';
