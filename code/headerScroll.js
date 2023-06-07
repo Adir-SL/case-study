a403e7696a2a1195a5a2880703ef4a6863579801
Adir <slutzkis@gmail.com>
2023-06-07 16:06:05 +0300

    updates

--- code/headerScroll.js
+++ code/headerScroll.js
@@ -24,8 +24,8 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.opacity = '0';
-        if(document.querySelectorAll('section')[0].getBoundingClientRect().bottom < 0){
-            if(document.getElementsByClassName('whiteSection')[0].getBoundingClientRect().top < 2){
+        if(document.querySelectorAll('section')[0].getBoundingClientRect().bottom < 30){
+            if(document.getElementsByClassName('whiteSection')[0].getBoundingClientRect().top < 30){
                 if(document.querySelectorAll('section')[0].classList.contains("firstSection") == true){
                     document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(100%)";
                 }else{
