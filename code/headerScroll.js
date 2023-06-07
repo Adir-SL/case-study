4ab62dd075dc180bcff6952672d586181f5f86cf
Adir <slutzkis@gmail.com>
2023-06-07 14:00:34 +0300

    Update headerScroll.js

--- code/headerScroll.js
+++ code/headerScroll.js
@@ -12,7 +12,7 @@ function scrollCheck(){
             scrollPos = scrollTravel;
         }
     }
-    // && document.querySelectorAll('section')[0].classList.contains("firstSection") == true
+    
 
     if(document.getElementById('mainHeader').getBoundingClientRect().top == '0'){
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.transitionDelay = '300ms';
@@ -26,9 +26,17 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.opacity = '0';
         if(document.querySelectorAll('section')[0].getBoundingClientRect().bottom < 0){
             if(document.getElementsByClassName('whiteSection')[0].getBoundingClientRect().top < 2){
-                document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(100%)";
+                if(document.querySelectorAll('section')[0].classList.contains("firstSection") == true){
+                    document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(100%)";
+                }else{
+                    document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(0%)";
+            }
             }else{
-                document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(0%)";
+                if(document.querySelectorAll('section')[0].classList.contains("firstSection") == true){
+                    document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(0%)";
+                }else{
+                    document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(100%)";
+                }
             }
         }else{
             document.getElementById('mainHeader').querySelectorAll('.logoDiv')[0].style.filter = "invert(100%)";
