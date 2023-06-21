ecee358ff09212bf35ad14adee34e304416ad255
Adir <slutzkis@gmail.com>
2023-06-21 11:49:10 +0300

    updates

--- code/headerScroll-min.js
+++ code/headerScroll-min.js
@@ -4,7 +4,7 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.transitionDelay = '0s';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.transitionDelay = '0s';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.transitionDelay = '0s';
-        document.getElementById('mainHeader').style.pointerEvents = 'none';
+        // document.getElementById('mainHeader').style.pointerEvents = 'none';
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.opacity = '1';
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '0';
