6520d11bf2f01d1b3945b2f940d4c8c9c4a866a1
Adir <slutzkis@gmail.com>
2023-06-21 12:12:29 +0300

    Update headerScroll.js

--- code/headerScroll.js
+++ code/headerScroll.js
@@ -20,6 +20,8 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.transitionDelay = '0s';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.transitionDelay = '0s';
         // document.getElementById('mainHeader').style.pointerEvents = 'none';
+        document.getElementById('mainHeader').querySelectorAll('button')[0].style.pointerEvents = 'none';
+        document.getElementsByClassName("linksDiv")[0].style.pointerEvents = 'none';
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.opacity = '1';
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '0';
@@ -46,7 +48,9 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.transitionDelay = '300ms';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.transitionDelay = '300ms';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.transitionDelay = '0s';
-        document.getElementById('mainHeader').style.pointerEvents = 'all';
+        // document.getElementById('mainHeader').style.pointerEvents = 'all';
+        document.getElementById('mainHeader').querySelectorAll('button')[0].style.pointerEvents = 'all';
+        document.getElementsByClassName("linksDiv")[0].style.pointerEvents = 'all';
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '1';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '1';
