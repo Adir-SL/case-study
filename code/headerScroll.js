29a99115337b00495a51ddad3674adfcd33a5a9e
Adir <slutzkis@gmail.com>
2023-09-21 17:13:50 +0300

    Update headerScroll.js

--- code/headerScroll.js
+++ code/headerScroll.js
@@ -20,8 +20,8 @@ function scrollCheck(){
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.transitionDelay = '0s';
         document.getElementById('mainHeader').querySelectorAll('button')[0].style.transitionDelay = '0s';
         // document.getElementById('mainHeader').style.pointerEvents = 'none';
-        document.getElementById('mainHeader').querySelectorAll('button')[0].style.pointerEvents = 'none';
-        document.getElementsByClassName("linksDiv")[0].style.pointerEvents = 'none';
+        // document.getElementById('mainHeader').querySelectorAll('button')[0].style.pointerEvents = 'none';
+        // document.getElementsByClassName("linksDiv")[0].style.pointerEvents = 'none';
         document.getElementById('mainHeader').querySelectorAll('div')[0].style.opacity = '1';
         document.getElementById('mainHeader').querySelectorAll('div')[1].style.opacity = '0';
         document.getElementById('mainHeader').querySelectorAll('div')[2].style.opacity = '0';
